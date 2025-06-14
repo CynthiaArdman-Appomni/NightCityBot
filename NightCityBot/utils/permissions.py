a4c2261519e56cb30b29580edc33d6f53b959b96
Cynthia Ardman <cardman@appomni.com>
2025-06-14 16:39:52 -0700

    Fix JSON helper to accept string paths

--- NightCityBot/utils/permissions.py
+++ NightCityBot/utils/permissions.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+import discord
 import config
 
 
@@ -21,5 +22,27 @@ def is_fixer():
                 return False
 
         return discord.utils.get(member.roles, name=config.FIXER_ROLE_NAME) is not None
+
+    return commands.check(predicate)
+
+
+def is_ripperdoc():
+    """Check that the command author has the Ripperdoc role."""
+
+    async def predicate(ctx):
+        guild = ctx.bot.get_guild(config.GUILD_ID)
+        if not guild:
+            return False
+
+        member = ctx.author
+        if not isinstance(member, discord.Member):
+            member = guild.get_member(ctx.author.id)
+            if not member:
+                try:
+                    member = await guild.fetch_member(ctx.author.id)
+                except discord.NotFound:
+                    return False
+
+        return any(r.id == config.RIPPERDOC_ROLE_ID for r in getattr(member, "roles", []))
 
     return commands.check(predicate)
